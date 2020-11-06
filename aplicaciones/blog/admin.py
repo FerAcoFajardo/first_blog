32ae7ad00d1742029033265d93620f6fee5a5cc4
Fernando <fernando.acosta216555@potros.itson.edu.mx>
2020-11-05 23:12:44 -0700

    Impoertar y exportar

--- aplicaciones/blog/admin.py
+++ aplicaciones/blog/admin.py
@@ -23,7 +23,17 @@ class AutorAdmin(ImportExportModelAdmin,admin.ModelAdmin):
     list_display = ('nombre','apellidos','descripcion','email','facebook','twitter','github','web','estado','fecha_creacion',)
     resources_class = AutorResources
 
+    
+class PostResources(resources.ModelResource):
+    class Meta:
+        model = Post
+
+class PostAdmin(ImportExportModelAdmin,admin.ModelAdmin):
+    search_fields = ['titulo','slug','descripcion']
+    list_display = ('titulo','slug','descripcion','fecha_creacion')
+    resources_class = PostResources
+
 # Register your models here.
 admin.site.register(Categoria,CategoriaAdmin)
 admin.site.register(Autor,AutorAdmin)
-admin.site.register(Post)
+admin.site.register(Post,PostAdmin)
