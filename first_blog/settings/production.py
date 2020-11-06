0b0d16a86b3a5f44473f0e94f5f70b7297c66589
Fernando <fernando.acosta216555@potros.itson.edu.mx>
2020-11-05 22:09:56 -0700

    Migracion a postgre y solucion de errores

--- first_blog/settings/production.py
+++ first_blog/settings/production.py
@@ -7,7 +7,11 @@ ALLOWED_HOSTS = ['djangoblogfernando.herokuapp.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'depgcmfi2u36md',
+        'USER': 'wuxxrcastvjfnu',
+        'PASSWORD': '0cb82be425974c8e0d02871928f0916ac714aca785f9874d616d104b2cdfe043',
+        'HOST': 'ec2-54-158-190-214.compute-1.amazonaws.com',
+        'PORT': 5432,
     }
 }
